d4ec05caba57f653428ebba790e00b6c38aa9e88
unknown <jacob.segerstedt@AWD510W22.CM.lan>
2015-06-09 10:57:50 +0200

    massa skit

---
+++ Assets/Scripts/World/GroupVegetationHit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroupVegetationHit : MonoBehaviour {
+
+	void OnTriggerEnter() {
+		foreach (Transform child in transform) {
+			child.SendMessage("TriggerWobble", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+}
