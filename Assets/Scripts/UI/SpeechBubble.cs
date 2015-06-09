569ba8ad1131604825d71370bf39bcdbf4f4ded1
unknown <ivan.hook@WH9314BC.CM.lan>
2015-06-09 16:52:49 +0200

    speechbubbles added

---
+++ Assets/Scripts/UI/SpeechBubble.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class SpeechBubble : MonoBehaviour {
+
+    Camera cam;
+
+    public float messageDuration = 2f;
+    float scaleDuration = 0.5f;
+
+	void Start () {
+        cam = Camera.main;
+
+        Vector3 fwd = cam.transform.forward;
+        transform.rotation = Quaternion.LookRotation(fwd);
+
+        transform.localScale = Vector3.zero;
+
+        transform.DOScale(Vector3.one, scaleDuration);
+
+        StartCoroutine(Remove());
+
+	}
+
+    IEnumerator Remove() {
+        yield return new WaitForSeconds(messageDuration - scaleDuration);
+        transform.DOScale(Vector3.zero, scaleDuration);
+        Destroy(gameObject, scaleDuration);
+    }
+
+}
