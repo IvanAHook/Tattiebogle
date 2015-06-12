92de6e4cc287d3f6697edcae35c8e25413e4b85f
unknown <kalle.nystrom@WH931623.CM.lan>
2015-06-12 10:20:02 +0200

    Kalles the menuman

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,13 +4,28 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 
 	public GameObject mainMenu;
-	
+
+	public Animator anim;
+
+	public Animator fade;
 
 	public void StartGame(){
-        Application.LoadLevel("MainLevelImplementingStuff");
+		StartCoroutine (StartGame2());
+	}
+
+	public IEnumerator StartGame2(){
+		anim.SetTrigger ("LookUp");
+		yield return new WaitForSeconds (4f);
+		fade.SetTrigger ("Fade");
+		yield return new WaitForSeconds (1f);
 	}
 	
 	public void QuitGame(){
 		Application.Quit();
 	}
+
+	public void LoadAfterFade(){
+		Application.LoadLevel("MainLevelImplementingStuff");
+	}
+	
 }
